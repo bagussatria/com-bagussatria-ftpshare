356eb7e9b49f7eb71fe7504e557dc9fe308b692c
Bagus Satria (agas) <bagussatriaid@gmail.com>
2019-01-23 17:03:56 +0700

    new code

---
+++ wifi_data_sharing/build/generated/source/buildConfig/debug/com/sharing/file/data/ftp/transfer/free/ftpshare/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sharing.file.data.ftp.transfer.free.ftpshare;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sharing.file.data.ftp.transfer.wifi.server.free";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "1.6";
+}
